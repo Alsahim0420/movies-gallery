12ce095167c1b36e3f6db3b9dbbf70557b7b6032
Pablo Melo Mac <panmecar@gmail.com>
2024-01-11 12:06:25 -0500

    fix(design):primary color and month

--- lib/presentation/views/movies/home_view.dart
+++ lib/presentation/views/movies/home_view.dart
@@ -4,102 +4,120 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
-
 class HomeView extends ConsumerStatefulWidget {
-  const HomeView({ super.key });
+  const HomeView({super.key});
 
   @override
   HomeViewState createState() => HomeViewState();
 }
 
-class HomeViewState extends ConsumerState<HomeView> with AutomaticKeepAliveClientMixin {
-
+class HomeViewState extends ConsumerState<HomeView>
+    with AutomaticKeepAliveClientMixin {
   @override
   void initState() {
     super.initState();
-    
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
-    ref.read( popularMoviesProvider.notifier ).loadNextPage();
-    ref.read( topRatedMoviesProvider.notifier ).loadNextPage();
-    ref.read( upcomingMoviesProvider.notifier ).loadNextPage();
-  }
 
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
 
     final initialLoading = ref.watch(initialLoadingProvider);
-    if ( initialLoading ) return const FullScreenLoader();
-    
-    final slideShowMovies = ref.watch( moviesSlideshowProvider );
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    final topRatedMovies = ref.watch( topRatedMoviesProvider );
-    final upcomingMovies = ref.watch( upcomingMoviesProvider );
+    if (initialLoading) return const FullScreenLoader();
 
-    return CustomScrollView(
-      slivers: [
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
 
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-          ),
+    return CustomScrollView(slivers: [
+      const SliverAppBar(
+        floating: true,
+        flexibleSpace: FlexibleSpaceBar(
+          title: CustomAppbar(),
         ),
+      ),
+      SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+        return Column(
+          children: [
+            // const CustomAppbar(),
 
+            MoviesSlideshow(movies: slideShowMovies),
 
-        SliverList(delegate: SliverChildBuilderDelegate(
-          (context, index) {
-              return Column(
-                  children: [
-              
-                    // const CustomAppbar(),
-              
-                    MoviesSlideshow(movies: slideShowMovies ),
-              
-                    MovieHorizontalListview(
-                      movies: nowPlayingMovies,
-                      title: 'En cines',
-                      subTitle: 'Lunes 20',
-                      loadNextPage: () =>ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-                      
-                    ),
-              
-                    MovieHorizontalListview(
-                      movies: upcomingMovies,
-                      title: 'Próximamente',
-                      subTitle: 'En este mes',
-                      loadNextPage: () =>ref.read(upcomingMoviesProvider.notifier).loadNextPage()
-                    ),
-              
-                    //* Ya no estará aquí, ahora es parte del menú inferior
-                    // MovieHorizontalListview(
-                    //   movies: popularMovies,
-                    //   title: 'Populares',
-                    //   // subTitle: '',
-                    //   loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
-                    // ),
-              
-                    MovieHorizontalListview(
-                      movies: topRatedMovies,
-                      title: 'Mejor calificadas',
-                      subTitle: 'Desde siempre',
-                      loadNextPage: () =>ref.read(topRatedMoviesProvider.notifier).loadNextPage()
-                    ),
+            MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'En cines',
+                subTitle:
+                    "${DateTime.now().day} ${_getmonth(DateTime.now().month)}",
+                loadNextPage: () =>
+                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
 
-                    const SizedBox( height: 10 ),
-              
-              
-                  ],
-                );
-          },
-          childCount: 1
-        )),
+            MovieHorizontalListview(
+                movies: upcomingMovies,
+                title: 'Próximamente',
+                subTitle: 'En este mes',
+                loadNextPage: () =>
+                    ref.read(upcomingMoviesProvider.notifier).loadNextPage()),
 
-      ]
-    );
+            //* Ya no estará aquí, ahora es parte del menú inferior
+            // MovieHorizontalListview(
+            //   movies: popularMovies,
+            //   title: 'Populares',
+            //   // subTitle: '',
+            //   loadNextPage: () =>ref.read(popularMoviesProvider.notifier).loadNextPage()
+            // ),
+
+            MovieHorizontalListview(
+                movies: topRatedMovies,
+                title: 'Mejor calificadas',
+                subTitle: 'Desde siempre',
+                loadNextPage: () =>
+                    ref.read(topRatedMoviesProvider.notifier).loadNextPage()),
+
+            const SizedBox(height: 10),
+          ],
+        );
+      }, childCount: 1)),
+    ]);
   }
-  
+
+  String _getmonth(int month) {
+    switch (month) {
+      case 1:
+        return "Enero";
+      case 2:
+        return "Febrero";
+      case 3:
+        return "Marzo";
+      case 4:
+        return "Abril";
+      case 5:
+        return "Mayo";
+      case 6:
+        return "Junio";
+      case 7:
+        return "Julio";
+      case 8:
+        return "Agosoto";
+      case 9:
+        return "Septiembre";
+      case 10:
+        return "Octubre";
+      case 11:
+        return "Noviembre";
+      case 12:
+        return "Diciembre";
+      default:
+        return "No-data";
+    }
+  }
+
   @override
   bool get wantKeepAlive => true;
 }
